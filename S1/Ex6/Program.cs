86e1348b9204777d76022c0a281a379658d76e74
Evgeniia <homochk@yandex.ru>
2022-06-26 01:59:20 +0300

    Задача6

---
+++ S1/Ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число:");
+int n= Convert.ToInt32(Console.ReadLine());
+
+if (n%2==0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
