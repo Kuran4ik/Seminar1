4e8b2589a87fdb3324f50998e2dc04b78947b154
Evgeniia <homochk@yandex.ru>
2022-06-26 02:42:51 +0300

    Задача8

--- S1/Ex8/Program.cs
+++ S1/Ex8/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine ("Введите число:");
 int N= Convert.ToInt32(Console.ReadLine());
 int i=1;
+Console.WriteLine("Все четные числа от 1 до N");
 while (i<N)
 {
     if (i%2==0)
     {
-        Console.WriteLine("Все четные числа от 1 до N");
         Console.WriteLine(i);
     }
     i ++;
