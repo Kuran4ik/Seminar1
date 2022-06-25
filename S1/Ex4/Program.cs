cdd76818ba7ef9ecb6b85c3f120354d6c3ac0cfd
Evgeniia <homochk@yandex.ru>
2022-06-26 01:49:10 +0300

    задача4

---
+++ S1/Ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите первое число:");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число:");
+int b= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите третье число:");
+int c= Convert.ToInt32(Console.ReadLine());
+
+if (a>b && a>c)
+{
+    Console.WriteLine("Максимальное число:");
+    Console.WriteLine(a);
+}
+else if (b>a && b>c)
+{
+    Console.WriteLine("Максимальное число:");
+    Console.WriteLine(b);
+}
+else if (c>a && c>b)
+{
+    Console.WriteLine("Максимальное число:");
+    Console.WriteLine(c);
+}
