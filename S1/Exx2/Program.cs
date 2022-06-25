76b9973d0e7455871373b577e56f35ef9654a80d
Evgeniia <homochk@yandex.ru>
2022-06-26 01:26:40 +0300

    Задача2

---
+++ S1/Exx2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите первое число:");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число:");
+int b= Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("Это большее число:");
+    Console.WriteLine(a);
+    Console.WriteLine("Это меньшее число:");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Это большее число:");
+    Console.WriteLine(b);
+    Console.WriteLine("Это меньшее число:");
+    Console.WriteLine(a);
+}
