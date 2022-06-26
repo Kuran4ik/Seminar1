b9a31929fd90710260202c947d713bebc689da7b
Evgeniia <homochk@yandex.ru>
2022-06-26 14:05:08 +0300

    Доп. задача1

---
+++ S1/Dop1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1. Пользователь вводит число N (N > 0). Программа должна вывести N единиц на экран.
+//N = 4 -> 1, 1, 1, 1
+//N = 2 -> 1, 1
+Console.WriteLine ("Введите число:");
+int N= Convert.ToInt32(Console.ReadLine());
+int i=1;
+Console.WriteLine("Решение:");
+if (N>0)
+{
+    while ((N-i)>=0)
+{
+    Console.WriteLine(1);
+    i ++;
+}
+}
+else
+{
+    Console.WriteLine("N должно быть больше нуля");
+}
