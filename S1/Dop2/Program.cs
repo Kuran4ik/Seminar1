50c24cacd296fef7b6822aae48bc990148306dde
Evgeniia <homochk@yandex.ru>
2022-06-26 14:22:04 +0300

    Доп. задача2

---
+++ S1/Dop2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2. Даны два числа a, b, такие что a < b. Вывести на экран сколько раз число a поместиться в числе b.
+Console.WriteLine ("Введите число a:");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число b:");
+int b= Convert.ToInt32(Console.ReadLine());
+
+if (a<b)
+{
+    Console.WriteLine("Число а поместится в числе b");
+    Console.WriteLine(b/a);
+    Console.WriteLine("раз");
+}
+else
+{
+    Console.WriteLine("Число а должно быть меньше числа b");
+    
+}
