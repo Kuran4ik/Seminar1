64f214c90f9f0aa234986a8906f8c284f4b39f7b
Evgeniia <homochk@yandex.ru>
2022-06-26 15:28:39 +0300

    Доп. задание4

---
+++ S1/Dop4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4. Дано N секунд. Вывести время в формате часы:минуты:секунды.
+//N = 72334 -> 20:5:34 
+Console.WriteLine ("Введите количество секунд:");
+int N= Convert.ToInt32(Console.ReadLine());
+int a; 
+int b; 
+int c;
+a = N / 3600;
+b = (N-a*3600) / 60;
+c = N-a*3600-b*60;
+Console.WriteLine("Время в формате часы:минуты:секунды:");
+Console.WriteLine(a);
+Console.WriteLine("часов");
+Console.WriteLine(b);
+Console.WriteLine("минут");
+Console.WriteLine(c);Console.WriteLine("секунд");
+
+
+
+
